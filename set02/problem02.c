9993054f5d34879328e3ec3fb169b9b9fd305a7f
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-10-17 11:48:45 +0530

    committed 10/17/2023, 11:48:45 AM

---
+++ set02/problem02.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input_side(float *s1, float *s2, float *s3)
+{
+    printf("enter the three sides\n");
+    scanf("%f", &*s1);
+    scanf("%f", &*s2);
+    scanf("%f", &*s3);
+}
+int process(float s1, float s2, float s3)
+{
+    int isscalene=0;
+if(s1==s2 && s2==s3){
+printf("\nthe triangle is an equilateral triangle");
+}
+else if(s1==s2||s2==s3||s1==s3){printf("\nthe triangle is an isoceles triangle");}
+else{printf("\nthe triangle is a scalene triangle"); isscalene=1;}
+return isscalene;
+}
+void output(int a, int b, int c, int isscalene)
+{
+    if(isscalene){printf("the sides are %f %f %f", a, b, c);}
+}
+int main()
+{
+float s1, s2, s3; int a, b, c, isscalene;
+input_side(&s1, &s2, &s3);
+a=s1; b=s2; c=s3;
+isscalene = process(s1, s2, s3);
+output(a, b, c, isscalene);
+return 0;
+}
