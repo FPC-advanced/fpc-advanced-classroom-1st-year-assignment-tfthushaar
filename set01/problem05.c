b9bdd056d9c74684d1d9a136204309ceb3d3fdf0
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-10-31 11:44:22 +0530

    committed 10/31/2023, 11:44:21 AM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+int input(){
+    int a,b,c;
+    printf("enter the three numbers: ");
+    scanf("%d, %d, %d", &a, );
+    scanf("%d", &b);
+    scanf("%d", &c);
+    return a;
+}
+int compare(int a, int b, int c){
+    int largest;
+    if(a>b || a>c){largest=a;}
+    else if(b>a||b>c){largest=b;}
+    else if(c>a||c>b){largest=c;}
+    return largest;
+}
+void output(int a, int b, int c, int largest){
+    printf("the largest number between %d, %d and %d is %d", a,b,c,largest);
+}
+int main()
+{
+    int a=input();
+    int b=input();
+    int c=input();
+    int largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
