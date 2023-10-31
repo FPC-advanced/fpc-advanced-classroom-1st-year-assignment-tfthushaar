726985c7d60752d762ad7677cbb6256da0d1832e
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-10-31 12:01:22 +0530

    committed 10/31/2023, 12:01:21 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -9,13 +9,16 @@ int input(){
     scanf("%d", &c);
     return a;
 }
-int compare(int a, int b, int c){
+int compare(int a, int b, int c) {
     int largest;
-    if(a>b || a>c){largest=a;}
-    else if(b>a||b>c){largest=b;}
-    else {largest=c;}
+    if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a && b >= c) {
+        largest = b;
+    } else {largest = c;}
     return largest;
 }
+
 void output(int a, int b, int c, int largest){
     printf("the largest number between %d, %d and %d is %d", a,b,c,largest);
 }
