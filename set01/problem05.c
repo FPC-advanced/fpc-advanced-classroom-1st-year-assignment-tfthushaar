262a1513e8cef1e21c08d0658054e08784487b8e
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-10-31 12:10:52 +0530

    committed 10/31/2023, 12:10:51 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,25 +1,33 @@
-#include<stdio.h>
-int input(){
-    int n;
-    printf("enter the three numbers: ");
-    scanf("%d", &n);
-    return n;
+#include <stdio.h>
+
+int input() {
+    int a;
+    printf("enter the first number: ");
+    scanf("%d", &a);
+    return a;
 }
+
 int compare(int a, int b, int c) {
     int largest;
-    if (a >= b && a >= c) {largest = a;}
-    else if (b >= a && b >= c) {largest = b;}
-    else {largest = c;}
+    if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a && b >= c) {
+        largest = b;
+    } else {
+        largest = c;
+    }
     return largest;
 }
 
-void output(int a, int b, int c, int largest){
-    printf("the largest number between %d, %d and %d is %d", a,b,c,largest);
+void output(int a, int b, int c, int largest) {
+    printf("the largest number between %d, %d and %d is %d", a, b, c, largest);
 }
-int main()
-{
-    int a,b,c=input();
-    int largest=compare(a,b,c);
-    output(a,b,c,largest);
+
+int main() {
+    int a = input();
+    int b = input();
+    int c = input();
+    int largest = compare(a, b, c);
+    output(a, b, c, largest);
     return 0;
 }
