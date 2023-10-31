0d220b6ff12f7789849cd759658bad6b933ee267
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-10-31 12:02:22 +0530

    committed 10/31/2023, 12:02:21 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -11,10 +11,9 @@ int input(){
 }
 int compare(int a, int b, int c) {
     int largest;
-    if (a >= b && a >= c) {
-        +argest = a;}
-         else if (b >= a && b >= c) {largest = b;}
-         else {largest = c;}
+    if (a >= b && a >= c) {largest = a;}
+    else if (b >= a && b >= c) {largest = b;}
+    else {largest = c;}
     return largest;
 }
 
