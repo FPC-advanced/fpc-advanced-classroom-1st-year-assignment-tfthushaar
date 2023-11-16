2d738b5f9b0023c70afec858655bf7b3304a041d
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-11-16 15:46:48 +0530

    committed 11/16/2023, 3:46:48 PM

---
+++ set03/problem06.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char* a, char* b){
+    
+
+}
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
