437e99291b3223de5aeb294ccc327208ba7ce4cb
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-12-05 13:33:47 +0530

    committed 12/5/2023, 1:33:46 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,52 +1,51 @@
-#include<stdio.h>
-#include<math.h>
-int input_array_size()
-{
-    int n;
-    printf("\nenter the size of the array: ");
-    scanf("%d", &n);
-    return n;
-}
-
-
-void input_array(int n, int a[n])
-{
-    for (int i = 0; i < n; i++) {
-        printf("Enter element %d : ", i + 1);
-        scanf("%d", &a[i]);
-        }
-}
-
-
-int sum_composite_numbers(int n, int a[n])
-{
-    int sum=0;
-    for (int i = 2; i <= sqrt(n); i+=2) {
-    if ((n % i == 0)) {
-     while (n % i == 0){
-     n /= i;
-        }
-    printf("%d\t", i);
-        }
-        else {
-        continue;
-         }
-        }
-    if (n > 1) {
-         printf("%d\t", n);
-            }
-            return sum+n;
-        }
-
-void output(int sum){
-    printf("\nthe sum of composite numbers is: %d \n",sum);
-}
-
+#include <stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
 int main()
 {
-    int n = input_array_size();
-    int a[n];
-    input_array(n, a);
-    int sum = sum_composite_numbers(n, a);
-    output(sum);
-    return 0;}
+  int n, sum;
+  n = input_array_size();
+  int a[n];
+  input_array(n, a);
+  sum = sum_composite_numbers(n, a);
+  output(sum);
+  return 0;
+}
+int input_array_size()
+{
+  int n;
+  printf("Enter the size of array\n");
+  scanf("%d", &n);
+  return n;
+}
+void input_array(int n, int a[n])
+{
+  int i;
+  printf("Enter the array elements\n");
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &a[i]);
+  }
+}
+int sum_composite_numbers(int n, int a[n])
+{
+  int i, j, sum = 0;
+  for (j = 0; j < n; j++)
+  {
+    for (i = 2; i <= a[j] / 2; i++)
+    {
+      if (a[j] % i == 0)
+      {
+        sum = sum + a[j];
+        break;
+      }
+    }
+  }
+  return sum;
+}
+void output(int sum)
+{
+  printf("Sum of Composite Number is %d", sum);
+}
