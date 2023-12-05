5c7bb94e3ecbcce375adc36aaca0c5aae779923a
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-12-05 13:24:22 +0530

    committed 12/5/2023, 1:24:22 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,9 +1,37 @@
-#include<stdio.h>
-void input_two_strings(char *string1, char *string2){
-    printf("enter the first string: \n");
+#include <stdio.h>
+
+void input_two_strings(char *string1, char *string2) {
+    printf("Enter the first string: ");
     scanf("%s", string1);
-    printf("\n enter the second string:\n");
+    printf("Enter the second string: ");
     scanf("%s", string2);
 }
-int stringcompare(char *string1, char *string2);
-void output(char *string1, char *string2, int result);
+
+int stringcompare(char *string1, char *string2) {
+    return strcmp(string1, string2);
+}
+
+void output(char *string1, char *string2, int result) {
+    printf("First String: %s\n", string1);
+    printf("Second String: %s\n", string2);
+
+    if (result == 0) {
+        printf("The strings are equal.\n");
+    } else if (result > 0) {
+        printf("The first string is greater than the second string.\n");
+    } else {
+        printf("The second string is greater than the first string.\n");
+    }
+}
+
+int main() {
+    char input_string1[100], input_string2[100];
+    input_two_strings(input_string1, input_string2);
+
+    int result = stringcompare(input_string1, input_string2);
+
+    output(input_string1, input_string2, result);
+
+    return 0;
+}
+
