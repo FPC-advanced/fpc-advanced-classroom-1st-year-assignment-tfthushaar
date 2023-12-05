10b2f73e98dd34a72a6b971babc11f4e04058edd
tfthushaar <thushaarr.bsc23@rvu.edu.in>
2023-12-05 13:34:57 +0530

    committed 12/5/2023, 1:34:56 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,36 +1,42 @@
-#include<stdio.h>
-#include<string.h>
-
-void input_string(char *a)
-{
-    printf("Enter a string: ");
-    scanf("%s", a);
-}
-
-void str_reverse(char *str, char *rev_str)
-{
-    int len = strlen(str);
-    for (int i = 0; i < len; i++)
-    {
-        rev_str[len - 1 - i] = str[i];
-    }
-    rev_str[len] = '\0';
-}
-
-void output(char *a, char *reverse_a)
-{
-    printf("\n\nOriginal string is: %s\n", a);
-    printf("Reversed string is: %s\n", reverse_a);
-}
-
+#include <stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
 int main()
 {
-    char input_str[100];   
-    char reversed_str[100];
-
-    input_string(input_str);
-    str_reverse(input_str, reversed_str);
-    output(input_str, reversed_str);
-
-    return 0;
+  char str[100], rev_str[100], a[100], reverse_a[100];
+  input_string(a);
+  int i;
+  for (i = 0; a[i] != '\0'; i++)
+  {
+    str[i] = a[i];
+  }
+  str[i] = '\0';
+  str_reverse(str, rev_str);
+  output(a, rev_str);
+  return 0;
+}
+void input_string(char *a)
+{
+  printf("Enter a string:\n");
+  scanf("%s", a);
+}
+void str_reverse(char *str, char *rev_str)
+{
+  int len = 0;
+  while (str[len] != '\0')
+  {
+    len++;
+  }
+  int i, j = 0;
+  for (i = len - 1; i >= 0; i--)
+  {
+    rev_str[j] = str[i];
+    j++;
+  }
+  rev_str[j] = '\0';
+}
+void output(char *a, char *reverse_a)
+{
+  printf("the reversed string of %s is %s\n", a, reverse_a);
 }
